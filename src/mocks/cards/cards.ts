2f3154f82a482110cc381c3484c7ea6066aea84b
Егор Безмен <egorbezmen@MacBook-Air-Egor.local>
2023-05-14 13:49:59 +0300

    добавил данные для  карточек

---
+++ src/mocks/cards/cards.ts
@@ -0,0 +1,66 @@
+import type { Card } from '@/mocks/cards/type'
+
+export const cards: Card[] = [
+  {
+    title: 'Adverts',
+    icon: require('@/assets/icons/adverts.svg')
+  },
+  {
+    title: 'Apps',
+    icon: require('@/assets/icons/apps.svg')
+  },
+  {
+    title: 'Code',
+    icon: require('@/assets/icons/code.svg')
+  },
+  {
+    title: 'Graph',
+    icon: require('@/assets/icons/graph.svg')
+  },
+  {
+    title: 'Link',
+    icon: require('@/assets/icons/link.svg')
+  },
+  {
+    title: 'Links',
+    icon: require('@/assets/icons/links.svg')
+  },
+  {
+    title: 'Monitor',
+    icon: require('@/assets/icons/monitor.svg')
+  },
+  {
+    title: 'Notes',
+    icon: require('@/assets/icons/notes.svg')
+  },
+  {
+    title: 'Point',
+    icon: require('@/assets/icons/point.svg')
+  },
+  {
+    title: 'Screen',
+    icon: require('@/assets/icons/screen.svg')
+  },
+  {
+    title: 'Search',
+    icon: require('@/assets/icons/search.svg')
+  },
+  {
+    title: 'Server',
+    icon: require('@/assets/icons/server.svg')
+  },
+  {
+    title: 'Shapes',
+    icon: require('@/assets/icons/shapes.svg')
+  },
+  {
+    title: 'Share',
+    icon: require('@/assets/icons/share.svg')
+  },
+  {
+    title: 'Touch',
+    icon: require('@/assets/icons/touch.svg')
+  },
+]
+
+console.log(cards.length)
