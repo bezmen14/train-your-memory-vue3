3e01ab61305af2d78823e238eaabfb02cadb06b7
Егор Безмен <egorbezmen@MacBook-Air-Egor.local>
2023-05-14 21:20:39 +0300

    установил пакет генерации id

--- src/composable/useCardItems.ts
+++ src/composable/useCardItems.ts
@@ -1,6 +1,7 @@
 import { reactive, ref, watch } from 'vue'
 import type { Card } from '@/mocks/cards/cards'
 import { cards } from '@/mocks/cards/cards'
+import { v4 as uuidv4 } from 'uuid'
 
 const cardItems = reactive<Card[]>([])
 
@@ -11,7 +12,7 @@ const countFounded = ref(0)
 watch(
   () => cardItems,
   (newValue) => {
-    countFounded.value = newValue.filter(card => card.isFound).length
+    countFounded.value = newValue.filter((card) => card.isFound).length
   },
   { deep: true }
 )
@@ -20,13 +21,13 @@ const shuffleCards = () => {
   cardItems.length = 0
   ;[...cards, ...cards]
     .sort(() => Math.random() - 0.5)
-    .forEach((item, index) => {
-      const currentItem: Card = { ...item, id: btoa(String(index)), isFlipped: true }
+    .forEach((item) => {
+      const currentItem: Card = { ...item, id: uuidv4(), isFlipped: true }
       cardItems.push(currentItem)
     })
 
-  setTimeout(()=> {
-    cardItems.forEach((el)=> el.isFlipped = false)
+  setTimeout(() => {
+    cardItems.forEach((el) => (el.isFlipped = false))
   }, 3500)
 }
 
@@ -37,15 +38,23 @@ const validateTwoCurrentCard = (item: Card): void => {
 
   if (validationCards.length === 2) {
     if (validationCards[0].title === validationCards[1].title) {
-      validationCards.forEach(item => (item.isFound = true))
+      validationCards.forEach((item) => (item.isFound = true))
       validationCards.length = 0
     } else {
       setTimeout(() => {
-        validationCards.forEach(item => (item.isFlipped = false))
+        validationCards.forEach((item) => (item.isFlipped = false))
         validationCards.length = 0
       }, 700)
     }
   }
 }
 
-export default { cardItems, countFounded, shuffleCards, validationCards, currentCards, setCurrentCards, validateTwoCurrentCard }
+export default {
+  cardItems,
+  countFounded,
+  shuffleCards,
+  validationCards,
+  currentCards,
+  setCurrentCards,
+  validateTwoCurrentCard
+}
