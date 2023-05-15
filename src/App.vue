8934fc3001f21900d89a4a3caa07b6bd022ab75f
Егор Безмен <egorbezmen@MacBook-Air-Egor.local>
2023-05-15 12:02:44 +0300

    правки стилей заголовка

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,15 @@ import CardList from '@/components/CardList.vue'
 import useCardItems from '@/composable/useCardItems'
 import { cards } from '@/mocks/cards/cards'
 
-const { countCardsFound, shuffleCards, validateTwoCurrentCard } = useCardItems
+const { countCardsFound, shuffleCards, validateTwoCurrentCard, cardItemList } = useCardItems
 </script>
 
 <template>
   <div class="container">
     <h1>Тренируй память</h1>
-    <h1 v-if="countCardsFound === cards.length">
-      Поздравляем! Вы прошли игру
-    </h1>
-    <p>Совпадений {{ `${countCardsFound / 2} из ${cards.length}` }}</p>
-    <button @click="shuffleCards">Начать заново</button>
+    <h3 v-if="countCardsFound === cardItemList.length">Поздравляем! Вы нашли все совпадения</h3>
+    <h3>Совпадений {{ `${countCardsFound / 2} из ${cards.length}` }}</h3>
+    <button v-text="'Начать заново'" @click="shuffleCards" />
     <CardList @selected="validateTwoCurrentCard" />
   </div>
 </template>
@@ -23,5 +21,38 @@ const { countCardsFound, shuffleCards, validateTwoCurrentCard } = useCardItems
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+button {
+  font-family: 'Open Sans', sans-serif;
+  appearance: none;
+  cursor: pointer;
+  border-radius: 5px;
+  background: white;
+  color: black;
+  padding: 8px 16px;
+  font-size: 16px;
+  border-color: black;
+}
+
+button:hover {
+  background: whitesmoke;
+}
+
+h1 {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 40px;
+  font-weight: normal;
+  line-height: 1;
+  padding: 10px 0;
+  margin: 0;
+}
+
+h3 {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 20px;
+  font-weight: normal;
+  padding: 10px 0;
+  margin-bottom: 10px;
 }
 </style>
