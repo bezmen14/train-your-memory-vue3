b6300c45f7e5abdcffafc0fb4183821fe78d8470
Егор Безмен <egorbezmen@MacBook-Air-Egor.local>
2023-05-15 09:56:47 +0300

    добавил текст о том что игрок прошел игру

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import CardList from '@/components/CardList.vue'
-import useCardItems from "@/composable/useCardItems";
+import useCardItems from '@/composable/useCardItems'
+import { cards } from '@/mocks/cards/cards'
+
 const { countCardsFound, shuffleCards, validateTwoCurrentCard } = useCardItems
 </script>
 
 <template>
   <div class="container">
     <h1>Тренируй память</h1>
-    <p>Совпадений {{`${countCardsFound / 2} из 15`}}</p>
+    <h1 v-if="countCardsFound === cards.length">
+      Поздравляем! Вы прошли игру
+    </h1>
+    <p>Совпадений {{ `${countCardsFound / 2} из ${cards.length}` }}</p>
     <button @click="shuffleCards">Начать заново</button>
     <CardList @selected="validateTwoCurrentCard" />
   </div>
